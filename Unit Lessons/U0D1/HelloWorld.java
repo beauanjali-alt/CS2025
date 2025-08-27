9ab1f671abe80814f8a69e2eeec637b7d6ffd60d
beauanjali-alt <beauanjali@gmail.com>
2025-08-27 09:16:53 -0700

    class notes day 1

---
+++ Unit Lessons/U0D1/HelloWorld.java
@@ -0,0 +1,39 @@
+public class HelloWorld { // A class is a collection of related attributes and behaviors
+    public static void main(String[] args) { 
+        // main means entry point for computer to run code
+        System.out.println("Hello World");
+        System.out.println("my Name is Beau");
+
+
+        int number = 5;
+        char letter = 'a';
+        boolean isTrue = true;
+        float decimal = 50.0f;
+        double  bigdecimal = 0.6;
+
+        byte age = 25;
+        short population = 5200;
+        long bigIntger = 10000;
+
+        String myName = "Beau";
+
+        System.out.println(myName.length());
+
+    //conditional statments
+    int value = 5;
+    if (value == 5) {
+        System.out.println(value);
+    }
+    int appleCount = 3;
+    if (appleCount == 5);{
+        System.out.println("You ate 5 apples.");   
+    } else if (appleCount == 10);{
+        System.out.println("You ate 10 apples.");
+    }else {
+        System.out.println.("")
+    }
+    
+
+    }
+}
+
